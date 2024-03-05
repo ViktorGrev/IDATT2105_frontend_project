212291827ce3dcbcbad80a18293d625db98445c2
viktor <viktog2210@gmail.com>
2024-03-05 11:39:59 +0100

    Adding directorys in main

---
+++ backend/backend/src/main/java/idatt2105/backend/Model/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String hash;
+    private LocalDate expirationDate;
+    private boolean isAdmin;
+}
