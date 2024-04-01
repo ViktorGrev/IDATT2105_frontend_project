2d93357ecb0bb56db44178d87a86c66fed2998a2
vekaste <vekaste@stud.ntnu.no>
2024-04-01 13:33:26 +0200

    feat: Created history components

---
+++ frontend/src/components/HistoryBoxComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="historyBox">
+      <div class="infoBox">
+        <slot name="infoBoxContent">
+          <!-- Default content for infoBox -->
+          <p>The art of war 2</p>
+          <p>25 Questions</p>
+        </slot>
+      </div>
+      <div class="creatorBox">
+        <slot name="creatorBoxContent">
+          <!-- Default content for creatorBox -->
+          Vekaste
+        </slot>
+      </div>
+    </div>
+  </template>
+
+<style scoped>
+.historyBox {
+    padding: 10px;
+    box-shadow: rgba(87, 174, 250, 0.25) 0px 25px 50px -12px;
+    display: flex;
+    border-radius: 1em;
+    flex-direction: column;
+    width: 100%;
+    cursor: pointer;
+    margin: 40px;
+}
+
+.infoBox {
+    flex-direction: column;
+    font-size: 20px;
+}
+.creatorBox {
+    color: rgb(21, 83, 137);
+}
+</style>
---
+++ frontend/src/components/history.vue
@@ -0,0 +1,93 @@
+<template>
+<main>
+        <div class="slideShow">
+            <Carousel>
+                <Slide v-for="slide in slides" :key="slide.id">
+                    <!-- Use the HistoryBox component inside the Slide -->
+                    <HistoryBox>
+                        <!-- Customize infoBoxContent slot -->
+                        <template #infoBoxContent>
+                        <p>{{ slide.info.title }}</p>
+                        <p>{{ slide.info.questions }} Questions</p>
+                        </template>
+                        <!-- Customize creatorBoxContent slot -->
+                        <template #creatorBoxContent>
+                        {{ slide.creator }}
+                        </template>
+                    </HistoryBox>
+                </Slide>
+
+                <template #addons>
+                <Navigation />
+                <Pagination />
+                </template>
+            </Carousel>
+        </div> 
+</main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  scroll-behavior: smooth;
+  font-family: 'Poppins', sans-serif;
+}
+
+.historyBox {
+    padding: 10px;
+    box-shadow: rgba(87, 174, 250, 0.25) 0px 25px 50px -12px;
+    display: flex;
+    border-radius: 1em;
+    flex-direction: column;
+    width: 100%;
+    cursor: pointer;
+}
+
+.infoBox {
+    flex-direction: column;
+    font-size: 20px;
+}
+.creatorBox {
+    color: rgb(21, 83, 137);
+}
+
+.test {
+    width: 1000px;
+    padding: 20px;
+}
+
+.slideShow {
+    padding: 20px;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Slide, Pagination } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+import HistoryBox from '../components/HistoryBoxComponent.vue'
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+    HistoryBox,
+    Navigation
+  },
+  data() {
+    return {
+      slides: [
+        { id: 1, info: { title: "The art of war 1", questions: 25 }, creator: "Vekaste" },
+        { id: 2, info: { title: "The art of war 2", questions: 25 }, creator: "Vekaste" },
+        { id: 3, info: { title: "The art of war 3", questions: 25 }, creator: "Vekaste" },
+        { id: 4, info: { title: "The art of war 4", questions: 25 }, creator: "Vekaste" },
+
+        // Add more slide data as needed
+      ]
+    };
+  }
+};
+</script>
